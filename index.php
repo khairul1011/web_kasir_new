fe272b30f4ec15d458c943be15b712cef96ae329
KHAIRUL ANUAR <erul0575@gmail.com>
2025-07-01 15:08:56 +0700

    rapikan file index

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
 <?php
-// web-kasir-new/index.php
-
-// Mulai output buffering dan sesi di paling atas
+//Output buffering 
 ob_start();
 // session_start();
 
-// Definisikan path dasar (sudah benar)
+// Definisi path dasar
 define('APP_ROOT_PATH', __DIR__);
 define('ADMIN_PATH', APP_ROOT_PATH . '/admin');
 define('MODULE_PATH', ADMIN_PATH . '/module');
 define('INCLUDE_PATH', ADMIN_PATH . '/template/includes');
 define('BASE_URL', '/web-kasir-new');
 
-// Sertakan config.php
 require_once APP_ROOT_PATH . '/config.php';
 
-// Sertakan kelas View (sudah benar)
+//Kelas View
 $view_file_path = APP_ROOT_PATH . '/fungsi/view/view.php';
 require_once $view_file_path;
 
-// Buat instance dari kelas View (sudah benar)
+//Instance dari kelas View
 $view = new View($db);
-
 
 if (isset($_GET['page']) && $_GET['page'] == 'laporan' && isset($_GET['export']) && $_GET['export'] == 'true') {
     $view->exportLaporanToExcel($_GET);
@@ -29,5 +25,4 @@ if (isset($_GET['page']) && $_GET['page'] == 'laporan' && isset($_GET['export'])
 
 include INCLUDE_PATH . '/home.php';
 
-// Kirim semua output yang sudah ditahan ke browser
 ob_end_flush();
