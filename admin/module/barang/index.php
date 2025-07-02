57903e55e165dd5f05e09b3f7d7a1549ea196ab1
KHAIRUL ANUAR <erul0575@gmail.com>
2025-07-02 10:29:09 +0700

    hapus outlet

--- admin/module/barang/index.php
+++ admin/module/barang/index.php
@@ -183,10 +183,10 @@ extract($pageData);
                             <label for="deskripsi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi Produk</label>
                             <textarea name="deskripsi" id="deskripsi" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tulis deskripsi produk di sini"></textarea>
                         </div>
-                        <div class="col-span-2 sm:col-span-1">
+                        <!-- <div class="col-span-2 sm:col-span-1">
                             <label for="outlet_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID Outlet (Opsional)</label>
                             <input type="number" name="outlet_id" id="outlet_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ID Outlet">
-                        </div>
+                        </div> -->
                     </div>
                     <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
